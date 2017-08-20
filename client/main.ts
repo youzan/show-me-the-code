33ef3ae464e61a2e87510bd462b90f3507f6f9a7
intellild <intellild@outlook.com>
2017-08-20 22:13:34 +0800

    update

--- client/main.ts
+++ client/main.ts
@@ -1,12 +1,9 @@
 import 'babel-polyfill';
 import Vue from 'vue';
-import VueSocketio from 'vue-socket.io';
 
 import App from './App';
 
 (window as any).require.config({ paths: { 'vs': 'static/vs' }});
-
-Vue.use(VueSocketio, '/socket.io');
 
 const vm = new Vue({
   el: '#app',
