d10fb1cc88789911fd39453c49f963af960bdc63
intellild <intellild@outlook.com>
2017-08-22 14:31:59 +0800

    fix

--- client/room/App.vue
+++ client/room/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="app">
-    <div class="header">
-      <div>
-        <v-select v-model="language" />
-      </div>
-    </div>
+    <md-toolbar>
+      <md-input-container>
+        <md-select name="language" id="language" v-model="language">
+          <md-option v-for="language in languages" :key="language" :value="language">{{ language }}</md-option>
+        </md-select>
+      </md-input-container>
+    </md-toolbar>
     <div class="content">
       <v-monaco ref="monaco" class="editor" v-model="code" :language="language" />
     </div>
@@ -16,20 +18,18 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import MonacoEditor from 'vue-monaco';
 
-import Select from './components/LanguageSelect';
-
+import { languages } from './config';
 import './style';
 
 @Component({
   components: {
-    'v-monaco': MonacoEditor,
-    'v-select': Select
+    'v-monaco': MonacoEditor
   }
-} as any)
+})
 export default class App extends Vue {
   code = `console.log('hello world');`
   language = 'javascript'
-
+  languages = languages
 
 }
 
--- client/room/main.ts
+++ client/room/main.ts
@@ -9,6 +9,17 @@ import App from './App';
 
 Vue.use(VueMaterial);
 
+(Vue as any).material.registerTheme('default', {
+  primary: {
+    color: 'white',
+    hue: 900,
+    textColor: 'black'
+  },
+  accent: 'grey',
+  warn: 'red',
+  background: 'white'
+});
+
 const vm = new Vue({
   el: '#app',
   render: h => h(App)
