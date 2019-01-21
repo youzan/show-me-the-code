8352b7e0c8f0f95f817a2a94cdb830df034e58d4
まきちゃん~ <intellild@outlook.com>
2019-01-21 11:08:13 +0800

    enable concurrent mode

--- client/main.tsx
+++ client/main.tsx
@@ -7,17 +7,17 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import App, { store, connection } from './app';
 
-// (ReactDOM as any).createRoot(document.getElementById('app')).render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   connection.connect.bind(connection),
-// );
-
-ReactDOM.render(
+(ReactDOM as any).createRoot(document.getElementById('app')).render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('app'),
   connection.connect.bind(connection),
 );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('app'),
+//   connection.connect.bind(connection),
+// );
