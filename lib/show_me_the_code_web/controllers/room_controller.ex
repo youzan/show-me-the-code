e03381ace63be5b6c1a7a9779c135bcc992e2b20
intellild <intellild@outlook.com>
2019-08-15 00:26:55 +0800

    create check params

--- lib/show_me_the_code_web/controllers/room_controller.ex
+++ lib/show_me_the_code_web/controllers/room_controller.ex
@@ -4,21 +4,47 @@ defmodule ShowMeTheCodeWeb.RoomController do
   alias Ecto.Multi
 
   def create_one(conn, params) do
-    expires = params[:expires]
-    {:ok, room} = Repo.insert(%Room{expires: expires})
-    json(conn, %{:response => room.id})
+    case pick_expires(params) do
+      {:ok, expires} ->
+        Repo.insert!(%Room{expires: expires})
+        |> (&json(conn, %{response: &1})).()
+
+      {:error, _} ->
+        send_resp(conn, 400, "invalid expires")
+    end
   end
 
-  def create_many(conn, %{"amount" => amount} = params) do
-    expires = params[:expires]
+  def create_many(conn, %{"amount" => amount} = params) when is_integer(amount) do
+    try do
+      expires =
+        case pick_expires(params) do
+          {:ok, expires} -> expires
+          {:error, _} -> throw(:invalid_expires)
+        end
 
-    multi =
-      Enum.reduce(1..amount, Multi.new(), fn index, multi ->
-        Multi.insert(multi, index, %Room{expires: expires})
-      end)
+      multi =
+        Enum.reduce(1..amount, Multi.new(), fn index, multi ->
+          Multi.insert(multi, index, %Room{expires: expires})
+        end)
 
-    {:ok, result} = Repo.transaction(multi)
-    response = Enum.map(result, fn {_, value} -> value.id end)
-    json(conn, %{:response => response})
+      {:ok, result} = Repo.transaction(multi)
+      response = Enum.map(result, fn {_, value} -> value.id end)
+      json(conn, %{:response => response})
+    catch
+      :invalid_expires -> send_resp(conn, 400, "invalid expires")
+    end
+  end
+
+  defp pick_expires(params) do
+    expires = params["expires"]
+
+    if expires != nil do
+      case NaiveDateTime.from_iso8601(expires) do
+        {:ok, expires} -> {:ok, NaiveDateTime.truncate(expires, :second)}
+        {:error, _} -> {:error, nil}
+      end
+    else
+      {:ok, nil}
+    end
   end
 end
