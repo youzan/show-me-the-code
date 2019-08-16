7f74c801689d85d1c1ce0523901946558254a47e
intellild <intellild@outlook.com>
2019-08-16 17:46:31 +0800

    implement theme switch

--- client/app/config.component.ts
+++ client/app/config.component.ts
@@ -9,7 +9,7 @@ import { EditorService } from './editor.service';
     <p-dropdown
       [options]="languageOptions"
       [ngModel]="language$ | async"
-      (ngModelChange)="langaugeChange($event)"
+      (ngModelChange)="languageChange($event)"
       [filter]="true"
     ></p-dropdown>
     <label>Font Size: </label>
@@ -18,26 +18,44 @@ import { EditorService } from './editor.service';
       [ngModel]="fontSize$ | async"
       (ngModelChange)="fontSizeChange($event)"
     ></p-dropdown>
+    <label>Theme: </label>
+    <div class="themes">
+      <p-radioButton
+        value="vs-dark"
+        label="Dark"
+        [ngModel]="theme$ | async"
+        (ngModelChange)="themeChange($event)"
+      ></p-radioButton>
+      <p-radioButton
+        value="vs-light"
+        label="Light"
+        [ngModel]="theme$ | async"
+        (ngModelChange)="themeChange($event)"
+      ></p-radioButton>
+    </div>
   `,
   styles: [
     `
       :host {
         display: grid;
-        grid-template:
-          'label1 language' 40px
-          'label2 fontSize' 40px;
         grid-template-columns: auto auto;
+        grid-auto-rows: minmax(40px, auto);
         align-items: center;
       }
 
       label {
         padding: 0 10px;
       }
+      
+      .themes {
+        display: flex;
+        justify-content: space-around;
+      }
     `,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfigCompoennt {
+export class ConfigComponent {
   readonly languageOptions = LANGUAGE_OPTIONS;
   readonly fontSizeOptions = FONT_SIZE_OPTIONS;
 
@@ -47,7 +65,7 @@ export class ConfigCompoennt {
     return this.editorService.language$;
   }
 
-  langaugeChange(lang: string) {
+  languageChange(lang: string) {
     this.editorService.language$.next(lang);
   }
 
@@ -57,5 +75,13 @@ export class ConfigCompoennt {
 
   fontSizeChange(fontSize: number) {
     this.editorService.fontSize$.next(fontSize);
+  }
+
+  get theme$() {
+    return this.editorService.theme$;
+  }
+
+  themeChange(theme: string) {
+    this.editorService.theme$.next(theme);
   }
 }
--- client/app/editor.service.ts
+++ client/app/editor.service.ts
@@ -12,7 +12,7 @@ import { BehaviorSubject, Subscription, Subject } from 'rxjs';
 //   jsx: monaco.languages.typescript.JsxEmit.None,
 // };
 
-monaco.editor.setTheme('vs-dark');
+const THEME_KEY = 'THEME';
 // monaco.languages.typescript.typescriptDefaults.setCompilerOptions(defaultTypeScriptCompilerOption);
 
 @Injectable()
@@ -21,15 +21,22 @@ export class EditorService implements OnDestroy {
   readonly fontSize$ = new BehaviorSubject(14);
   readonly expired$ = new BehaviorSubject(false);
   readonly format$ = new Subject();
+  readonly theme$: BehaviorSubject<string>;
   expires: Date | null = null;
 
   readonly model = monaco.editor.createModel('', this.language$.getValue());
   private $$: Subscription[] = [];
 
   constructor() {
+    const initialTheme = localStorage.getItem(THEME_KEY) || 'vs-dark';
+    this.theme$ = new BehaviorSubject(initialTheme);
     this.$$.push(
-      this.language$.subscribe(langauge => {
-        monaco.editor.setModelLanguage(this.model, langauge);
+      this.language$.subscribe(language => {
+        monaco.editor.setModelLanguage(this.model, language);
+      }),
+      this.theme$.subscribe(theme => {
+        localStorage.setItem(THEME_KEY, theme);
+        monaco.editor.setTheme(theme);
       }),
     );
   }
