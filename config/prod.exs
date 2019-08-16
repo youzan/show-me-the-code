89db6ed7a6c6d081564f94ec746b17737be87667
intellild <intellild@outlook.com>
2019-08-17 00:44:21 +0800

    fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :show_me_the_code, ShowMeTheCodeWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "socket.icode.live", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
